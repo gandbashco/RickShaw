edff6aaffca6f30b2520cf02902c5291779c1a8f
Moe B <moeb59@gmail.com>
2016-10-13 16:25:59 -0400

    Insert bootstrap data, some orders.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,1 +1,11 @@
 INSERT INTO CUSTOMER VALUES(1, 'John', 'Doe', 'jdoe@rickshaw.com', 'plainTextPassword!');
+INSERT INTO CUSTOMER VALUES(2, 'Moe', 'Stooge', 'moe@rickshaw.com', 'whyYouNumbskull');
+INSERT INTO CUSTOMER VALUES(3, 'Larry', 'Stooge', 'larry@rickshaw.com', 'whatsTheBigIdea?');
+INSERT INTO CUSTOMER VALUES(4, 'Curly', 'Stooge', 'curly@rickshaw.com', 'soitenly!');
+INSERT INTO CUSTOMER VALUES(5, 'Jagga', 'Daaku', 'jagga@lailpur.com', 'suitBootAndRickshaw');
+
+INSERT INTO `order` VALUES(1, '2016-07-08', 55.23, 2);
+INSERT INTO `order` VALUES(2, CURRENT_TIMESTAMP(), 7.45, 2);
+INSERT INTO `order` VALUES(3, '2015-07-08', 355.69, 3);
+INSERT INTO `order` VALUES(4, '2013-07-08', 888.55, 1);
+INSERT INTO `order` VALUES(5, '2012-07-08', 68.70, 5);
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS CUSTOMER;
-
-CREATE TABLE CUSTOMER(ID BIGINT PRIMARY KEY,
-                      FIRSTNAME VARCHAR(255) NOT NULL,
-                      LASTNAME VARCHAR(255) NOT NULL,
-                      EMAIL VARCHAR(255) NOT NULL,
-                      PASSWORD VARCHAR(255) NOT NULL
+CREATE TABLE CUSTOMER(id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                      firstname VARCHAR(255) NOT NULL,
+                      lastname VARCHAR(255) NOT NULL,
+                      email VARCHAR(255) NOT NULL,
+                      password VARCHAR(255) NOT NULL
               );
+
+DROP TABLE IF EXISTS `ORDER`;
+CREATE TABLE `ORDER`(id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                      date_created DATE NOT NULL,
+                      total DECIMAL NOT NULL,
+                      customer_id BIGINT NOT NULL,
+                      FOREIGN KEY (customer_id) references CUSTOMER(id),
+                    );
