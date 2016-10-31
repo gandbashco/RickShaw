a5e5308a4f3c64b0bcc76930ff3a3311d8aa0f0a
Moe B <moeb59@gmail.com>
2016-10-31 11:13:25 -0400

    Able to fetch all and one by id customer(s) now using proper method chaining i.e. controller calls service which calls the DAO.

--- src/main/java/com/rickshaw/controller/CustomerController.java
+++ src/main/java/com/rickshaw/controller/CustomerController.java
@@ -1,28 +1,41 @@
 package com.rickshaw.controller;
 
+import com.rickshaw.domain.Customer;
 import com.rickshaw.service.CustomerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+	/* Controller should be able to respond to requests for:
+	1. List all customers. DONE.
+	2. List one customer. DONE.
+	3. Create a customer.
+	4. Delete a customer.
+	5. Update a customer.
+	 */
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
-	private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
-	
-	@Autowired
-	CustomerService customerService;
+    private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
 
-	/* Controller should be able to respond to requests for:
-	1. List all customers.
-	2. List one customer.
-	3. Create a customer.
-	4. Delete a customer.
-	5. Update a customer.
-	 */
-	
+    @Autowired
+    CustomerService customerService;
 
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Collection<Customer> getAllCustomers() {
+        return customerService.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("id") long id) {
+        return customerService.getCustomerById(id);
+    }
 }
--- src/main/java/com/rickshaw/service/CustomerService.java
+++ src/main/java/com/rickshaw/service/CustomerService.java
@@ -1,24 +1,27 @@
 package com.rickshaw.service;
 
 import com.rickshaw.dao.CustomerDao;
+import com.rickshaw.domain.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class CustomerService {
 
     private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
 
-	/* Service should delegate requests for requests for:
-    1. List all customers.
-	2. List one customer.
-	3. Create a customer.
-	4. Delete a customer.
-	5. Update a customer.
-	 */
-
 	@Autowired
     private CustomerDao customerDao;
+
+    public Collection<Customer> getAll() {
+        return customerDao.getAll();
+    }
+
+    public Customer getCustomerById(long id) {
+        return customerDao.getById(id);
+    }
 }
