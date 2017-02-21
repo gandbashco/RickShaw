23bec490db47f4c233009fee46ecf19d97b537b3
Moe B <moeb59@gmail.com>
2017-02-20 20:45:17 -0500

    NON-WORKING: Experimenting with Lombok.

--- src/main/java/com/rickshaw/service/CustomerService.java
+++ src/main/java/com/rickshaw/service/CustomerService.java
@@ -2,23 +2,27 @@ package com.rickshaw.service;
 
 import com.rickshaw.repository.CustomerRepository;
 import com.rickshaw.domain.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+@Slf4j
 
 @Service
 public class CustomerService {
 
-    private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
+    //private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
 
 	@Autowired
     private CustomerRepository customerRepository;
 
     public Iterable<Customer> getAll() {
+        log.error("fake error");
         return customerRepository.findAll();
+
     }
 
     public Customer getCustomerById(Long id) {
