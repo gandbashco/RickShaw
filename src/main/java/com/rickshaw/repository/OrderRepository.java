06d930f37b9f58071a65f4e80ff4763922381130
Moe B <moeb59@gmail.com>
2017-03-27 12:41:32 -0400

    Add method to order repository to fetch orders by a given customer.

--- src/main/java/com/rickshaw/repository/OrderRepository.java
+++ src/main/java/com/rickshaw/repository/OrderRepository.java
@@ -1,9 +1,28 @@
 package com.rickshaw.repository;
 
+import com.rickshaw.domain.Customer;
+import com.rickshaw.domain.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Repository
 @Transactional
 public class OrderRepository {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Set<Order> findByCustomer(Long customerId) {
+        Set<Order> orders = new HashSet<>();
+        Optional<Customer> customer = Optional.ofNullable(customerRepository.findOne(customerId));
+        if (customer.isPresent()) {
+            orders =  customer.get().getOrders();
+        }
+        return orders;
+    }
 }
